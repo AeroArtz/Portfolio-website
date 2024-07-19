37062f99806c8d91eed996b4a969830dd39be100
AeroArtz <70429569+AeroArtz@users.noreply.github.com>
2024-07-19 10:02:10 +0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,7 +16,7 @@ export const About = () => {
                   I've been into programming since I was child in school . To me programming is not just a hobby, it's much more than that. I see it as bridge between dreams and reality, to be able to turn innovative ideas and dreams into reality.
               </p>
               <br></br>
-              <p>I started programming at the age of 15 when I first watched cs50, which ultimately led me to my journey. Ever since I've built numerous projects for myself and also professioanally for startups ranging from image classification of handwritten digits to self-driving car simulation using machine learning algorithms, as well as responsive full stakc web apps, mobile apps etc</p>
+              <p>I started programming at the age of 15 when I first watched cs50, which ultimately led me to my journey. Ever since I've built numerous perosnal projects ranging from image classification of handwritten digits to self-driving car simulation using machine learning algorithms. I've built projects professioanally for startups as well including responsive full stack web apps, mobile apps etc</p>
             </div>
         </div>
     </section>
